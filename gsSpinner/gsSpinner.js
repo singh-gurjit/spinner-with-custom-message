4cb8e627e66fc5fd8888a6591fb5ed2637eecca5
Gurjit Singh <gurjit.singh.contractor@pepsico.com>
2021-06-02 18:50:44 +0530

    changes

--- gsSpinner/gsSpinner.js
+++ gsSpinner/gsSpinner.js
@@ -5,31 +5,34 @@
  * @description Renders slds spinner with the custom wait message.
  */
 
- import { LightningElement, track, api } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
- var waitMessageLocal;
- var DELAY = 1000;
- var totalDotsToDisplay = 4; // will show 1 lesser dot than the value assiged here.
- 
- export default class GsSpinner extends LightningElement {
- 
-     @api waitMessage = 'Please wait';
-     interval;
- 
-     connectedCallback() {
-         waitMessageLocal = this.waitMessage;
-         let counter = 1;
-         this.interval = setInterval(() => {
-             if (counter % totalDotsToDisplay == 0) {
-                 this.waitMessage = waitMessageLocal;
-             } else {
-                 this.waitMessage += '.';
-             }
-             counter++;
-         }, DELAY)
-     }
- 
-     disconnectedCallback() {
-         clearInterval(this.interval);
-     }
- }
+var waitMessageLocal;
+var DELAY = 1000;
+var totalDotsToDisplay = 4; // will show 1 lesser dot than the value assiged here.
+
+export default class GsSpinner extends LightningElement {
+
+    @api waitMessage = 'Please wait';
+    interval;
+
+    connectedCallback() {
+        waitMessageLocal = this.waitMessage;
+        let counter = 1;
+
+        //Adding and removing the dots after wait message string in a fixed interval of time.
+        this.interval = setInterval(() => {
+            //Resetting the wait message with the original wait message without the dots.
+            if (counter % totalDotsToDisplay == 0) {
+                this.waitMessage = waitMessageLocal;
+            } else {
+                this.waitMessage += '.'; // Keep adding the dots to the wait message.
+            }
+            counter++;
+        }, DELAY)
+    }
+
+    disconnectedCallback() {
+        clearInterval(this.interval);
+    }
+}
